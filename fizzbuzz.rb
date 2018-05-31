dbc5750ecc3dc45bddc7ee6e478b0b3b6bdb0f0c
Richard Sanchez <richard.charles.s@gmail.com>
2018-05-31 23:27:20 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(x)
   if [x % 3 == 0 && x % 5 == 0]
     "FizzBuzz"
-  elsif [x % 3 == 0] ![x % 5 == 0]
+  elsif [x % 3 == 0] & ![x % 5 == 0]
      "Fizz"
-  elsif [x % 5 == 0] ![x % 3 == 0]
+  elsif [x % 5 == 0] & ![x % 3 == 0]
      "Buzz"
   end
 end
